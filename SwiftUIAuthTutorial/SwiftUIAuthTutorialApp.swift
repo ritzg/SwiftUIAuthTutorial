f7f8a1febc4e41830c35fdcd8a917346e94c9f14
Glenn Ritz <glennritz@gmail.com>
2024-01-05 15:07:15 -0500

    Initial Commit

---
+++ SwiftUIAuthTutorial/SwiftUIAuthTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAuthTutorialApp.swift
+//  SwiftUIAuthTutorial
+//
+//  Created by Glenn Ritz on 1/5/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAuthTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
